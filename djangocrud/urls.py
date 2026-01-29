81c29158aae3d4366f19a4a93bef9f05275e1d99
xBOOMERx07 <jeanpierre9930@gmail.com>
2026-01-28 21:34:41 -0500

    Fix: Eliminar serve de media en producción

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -1,20 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
-    # 1. Ruta del Administrador
     path('admin/', admin.site.urls),
-    
-    # 2. Rutas de tu aplicación (Tasks, CV, Venta Garage)
     path('', include('tasks.urls')),
-    
-    # 3. Regla de Oro para Render: Sirve archivos MEDIA (fotos) incluso si DEBUG es False
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-# Configuración extra para desarrollo local
+# Solo servir archivos media en desarrollo local
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
