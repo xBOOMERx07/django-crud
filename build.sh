e14c097318fb0d733da6cdeca39a7b2b6d6407df
xBOOMERx07 <jeanpierre9930@gmail.com>
2026-01-29 19:19:35 -0500

    Use migrate with run-syncdb to create missing tables

--- build.sh
+++ build.sh
@@ -5,9 +5,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-# FORZAR MIGRACIÓN: Primero borrar el registro de migraciones
-python manage.py migrate --fake tasks zero
-python manage.py migrate tasks
-
-# Migrar el resto
-python manage.py migrate
+# Sincronizar migraciones SIN borrar tablas existentes
+python manage.py migrate --run-syncdb
