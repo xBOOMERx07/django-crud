c1e1957fb50be12ab3213951a11a3e1f8da0cdb5
xBOOMERx07 <jeanpierre9930@gmail.com>
2026-01-29 19:14:10 -0500

    Force migrations reset

--- build.sh
+++ build.sh
@@ -4,4 +4,10 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+# FORZAR MIGRACIÓN: Primero borrar el registro de migraciones
+python manage.py migrate --fake tasks zero
+python manage.py migrate tasks
+
+# Migrar el resto
 python manage.py migrate
